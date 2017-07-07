3635982d2601676dc9287b38f364ad2aede31813
Tim Massingham <tim.massingham@nanoporetech.com>
2017-07-07 15:34:28 +0100

    Fast vectorised approximations to all activation functions

--- src/util.h
+++ src/util.h
@@ -19,6 +19,24 @@
 #        define EXPFV expfv
 #    endif
 
+#    ifdef FAST_TANH
+#        define TANHFV fast_tanhfv
+#    else
+#        define TANHFV tanhfv
+#    endif
+
+#    ifdef FAST_LOGISTICH
+#        define LOGISTICFV fast_logisticfv
+#    else
+#        define LOGISTICFV logisticfv
+#    endif
+
+#    ifdef FAST_ELUH
+#        define ELUFV fast_elufv
+#    else
+#        define ELUFV elufv
+#    endif
+
 /* Create a vector of  ones.  */
 extern __inline __m128 __attribute__ ((__gnu_inline__, __always_inline__))
     _mm_setone_ps(void) {
@@ -32,6 +50,10 @@ extern __inline __m128d __attribute__ ((__gnu_inline__, __always_inline__))
     1.0, 1.0};
 }
 
+
+/**
+ *   Standard implementations
+ **/
 static inline float logisticf(float x) {
     return 1.0 / (1.0 + expf(-x));
 }
@@ -62,16 +84,6 @@ static inline float fast_expf(float x) {
     return value.f;
 }
 
-static inline __m128 fast_expfv(__m128 x) {
-    const __m128 a = (__m128) (__v4sf) { _A, _A, _A, _A };
-    const __m128 b = (__m128) (__v4sf) { _B, _B, _B, _B };
-    const __m128 _bound = (__m128) (__v4sf) { _BOUND, _BOUND, _BOUND, _BOUND };
-    x = _mm_max_ps(-_bound, _mm_min_ps(_bound, x));
-
-    __m128 y = a * x + b;
-    return _mm_castsi128_ps(_mm_cvtps_epi32(y));
-}
-
 static inline float fast_logisticf(float x) {
     return 1.0 / (1.0 + fast_expf(-x));
 }
@@ -85,13 +97,21 @@ static inline float fast_eluf(float x){
     return (x >= 0.0f) ? x : (fast_expf(x) - 1.0);
 }
 
+/**
+ *   Vectorised functions
+ **/
 static inline __m128 __attribute__ ((__always_inline__)) expfv(__m128 x) {
     __v4sf y = (__v4sf) x;
     return (__m128) exp_ps(y);
 }
 
+static inline __m128 __attribute__ ((__always_inline__)) logfv(__m128 x) {
+    __v4sf y = (__v4sf) x;
+    return (__m128) log_ps(y);
+}
+
 static inline __m128 __attribute__ ((__always_inline__)) logisticfv(__m128 x) {
-    return _mm_rcp_ps(_mm_add_ps(_mm_setone_ps(), EXPFV(-x)));
+    return _mm_rcp_ps(_mm_add_ps(_mm_setone_ps(), expfv(-x)));
 }
 
 static inline __m128 __attribute__ ((__always_inline__)) tanhfv(__m128 x) {
@@ -109,6 +129,19 @@ static inline __m128 __attribute__ ((__always_inline__)) elufv(__m128 x) {
     return _mm_or_ps(_mm_and_ps(mask, x),  _mm_andnot_ps(mask, y));
 }
 
+/**
+ *    Fast vectorised approximations
+ **/
+static inline __m128 fast_expfv(__m128 x) {
+    const __m128 a = (__m128) (__v4sf) { _A, _A, _A, _A };
+    const __m128 b = (__m128) (__v4sf) { _B, _B, _B, _B };
+    const __m128 _bound = (__m128) (__v4sf) { _BOUND, _BOUND, _BOUND, _BOUND };
+    x = _mm_max_ps(-_bound, _mm_min_ps(_bound, x));
+
+    __m128 y = a * x + b;
+    return _mm_castsi128_ps(_mm_cvtps_epi32(y));
+}
+
 static inline __m128 fast_logfv(__m128 x) {
 #    define _Alogfv 8.262958294867817e-08f
 #    define _Blogfv 1064872507.1541044f
@@ -118,9 +151,23 @@ static inline __m128 fast_logfv(__m128 x) {
     return a * (x - b);
 }
 
-static inline __m128 logfv(__m128 x) {
-    __v4sf y = (__v4sf) x;
-    return (__m128) log_ps(y);
+static inline __m128 __attribute__ ((__always_inline__)) fast_logisticfv(__m128 x) {
+    return _mm_rcp_ps(_mm_add_ps(_mm_setone_ps(), fast_expfv(-x)));
+}
+
+static inline __m128 __attribute__ ((__always_inline__)) fast_tanhfv(__m128 x) {
+    const __m128 y = fast_logisticfv(_mm_add_ps(x, x));
+    return _mm_sub_ps(_mm_add_ps(y, y), _mm_setone_ps());
+}
+
+static inline __m128 __attribute__ ((__always_inline__)) fast_elufv(__m128 x) {
+    if(0 == _mm_movemask_ps(x)){
+        // All positive, early return.
+        return x;
+    }
+    const __m128 mask = _mm_cmpge_ps(x, _mm_setzero_ps());
+    const __m128 y = fast_expfv(x) - _mm_setone_ps();
+    return _mm_or_ps(_mm_and_ps(mask, x),  _mm_andnot_ps(mask, y));
 }
 
 char *strip_filename_extension(char *filename);
